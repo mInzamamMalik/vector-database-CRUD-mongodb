967c8e029e7c62f80b2ab9c6d6916f0a8d507138
M Inzamam Malik <malikasinger1@gmail.com>
2023-08-21 13:00:52 +0500

    mongodb attached

---
+++ mongodb.mjs
@@ -0,0 +1,21 @@
+import { MongoClient } from 'mongodb'
+
+export const client = new MongoClient(process.env.MONGODB_URI);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to Atlas");
+    } catch (err) {
+        console.log(err);
+        await client.close();
+        process.exit(1)
+    }
+}
+run().catch(console.dir);
+
+process.on('SIGINT', async function () {
+    console.log("app is terminating");
+    await client.close();
+    process.exit(0);
+});
